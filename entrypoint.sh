ab29a4b7b07a3ed6621061f9637128bfdcb66530
DolevTB <dolevgeva@gmail.com>
2025-03-26 15:16:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Ensure the virtual environment is activated
-source /opt/venv/bin/activate
+# Change to the app directory
+cd /app
+
+# Activate virtual environment
+source /app/venv/bin/activate
 
 # Wait for Redis to be available
 /usr/local/bin/wait-for-it.sh redis-server:6379 --timeout=60 --strict -- echo "Redis is up"
 
 # Run upgrade script
 echo 'Running upgrade.sh...'
-bash /opt/upgrade.sh
+bash /app/upgrade.sh
 
 # Start Gunicorn
 gunicorn --bind 0.0.0.0:8000 statuspage.wsgi:application \
-    --chdir /opt/statuspage \
+    --chdir /app \
     --workers 3 \
     --log-level debug
